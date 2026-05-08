6ef28dd6100ebc5bcd79be8e9ba7251676f066f6
Rique <henriquegoncm@gmail.com>
2026-05-08 07:18:44 -0300

    Update API_URL
    Temporary API URL

--- visalay-novo/src/App.jsx
+++ visalay-novo/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   // ==========================================
   // 2. DADOS LOCAIS (MOCK) E CONFIGURAÇÕES
   // ==========================================
-  const API_URL = 'http://localhost:3000'; 
+  const API_URL = "https://visalayapi.onrender.com";
 
   const alertasAnomalias = [
     { id: 1, tipo: 'Atraso', msg: 'Furadeira Bosch não devolvida por Carlos Eduardo.', hora: '17:05h' },
